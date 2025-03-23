64e1de2456f712951646a3c8f88064b31dfc56f2
sailing <caosenmail@gmail.com>
2025-03-23 15:37:52 +0800

    Update Config.php
    upgrade to PHP8, bug fixed

--- app/modules/config/src/Config.php
+++ app/modules/config/src/Config.php
@@ -181,6 +181,7 @@ class Config implements \ArrayAccess, \Countable, \JsonSerializable
      *
      * @see get()
      */
+    #[\ReturnTypeWillChange]
     public function offsetGet($key)
     {
         return $this->get($key);
