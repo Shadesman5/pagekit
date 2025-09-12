aa75dc5d307a1b70cfacfc7d2e28535208da465c
Shadesman <n.karpov90@gmail.com>
2025-09-12 22:55:40 +0200

    fix: Update UrlGenerator template for PHP 8.4 cache compatibility

--- app/modules/routing/src/Generator/UrlGeneratorDumper.php
+++ app/modules/routing/src/Generator/UrlGeneratorDumper.php
@@ -45,7 +45,7 @@ class {$options['class']} extends {$options['base_class']}
     /**
      * Constructor.
      */
-    public function __construct(RequestContext \$context, LoggerInterface \$logger = null)
+    public function __construct(RequestContext \$context, ?LoggerInterface \$logger = null)
     {
         \$this->context = \$context;
         \$this->logger = \$logger;
